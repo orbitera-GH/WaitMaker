33d6521ab39cba3e28f110ee236d8b2ff1818965
harn123 <harn@wp.pl>
2015-08-04 12:29:30 +0200

    ww

---
+++ WaitMaker.ps1
@@ -0,0 +1,13 @@
+﻿configuration WaitMaker 
+{ 
+  
+$seconds = 33
+$log = "c:\windows\temp\WaitMaker.txt"
+date >> $log
+echo "WaitMaker start" >> $log
+echo "Start sleep... $seconds" >> $log
+start-sleep -s $seconds
+date >> $log
+echo "WaitMaker stop" >> $log
+
+} 
