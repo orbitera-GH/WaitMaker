087774ebe11b4df9bcdde7b4cbe0145b518c1234
harn123 <harn@wp.pl>
2015-08-04 15:50:13 +0200

    qqq

--- getWaitMaker.ps1
+++ getWaitMaker.ps1
@@ -12,22 +12,5 @@ while ($ip.Length -lt 7) {
 	echo "(WaitMaker) Length: $z ip: $ip" >> $log
 	start-sleep -s 15
 }
-echo "Configure NetAppStorage.RDP" >> $log
-while ($exit -eq 0) {
-    if (Test-Path -Path "C:\Users\Public\Desktop") {
-		if (!(Test-Path -Path "C:\Users\Public\Desktop\NetAppStorage.RDP")) {
-			(Get-Content C:\Windows\OEM\NetAppStorage.RDP).Replace('10.10.1.23',"$ip") | Set-Content C:\Windows\OEM\NetAppStorage.RDP
-			Move-Item -Path C:\Windows\OEM\NetAppStorage.RDP -Destination C:\Users\Public\Desktop -Force
-			date >> $log
-			echo "(WaitMaker) NetAppStorage.RDP created " >> $log
-			$exit=1
-		} else {
-			date >> $log
-			echo "(WaitMaker) Waiting for directory..." >> $log
-		}
-    }else{
-        date >> $log
-        echo "(WaitMaker) Waiting for directory..." >> $log
-	    start-sleep -s 15
-    }
-}
+date >> $log
+echo "(WaitMaker) Script End" >> $log
