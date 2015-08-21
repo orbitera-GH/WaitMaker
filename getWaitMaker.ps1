81bdd85c88c4a84017cf6a309de86fcaca46811b
harn123 <harn@wp.pl>
2015-08-21 16:52:06 +0200

    ustawia nazwe sqla w hosts i wstawia nazwe do pliku RDP

--- getWaitMaker.ps1
+++ getWaitMaker.ps1
@@ -15,13 +15,16 @@ while ($ip.Length -lt 7) {
 date >> $log
 echo "(WaitMaker) Script End" >> $log
 
-$ip_port=$ip +":3395"
+#$ip_port=$ip +":3395"
+$ip_port=$ip +":3389"
 echo "change NetAppStorage.RDP" >> C:\Windows\Panther\get.log
+$sqlName=(new-object net.webclient).DownloadString('http://168.62.183.34/azurecheck.php?group='+$RG+'&getsqlname=true')
 while ($exit -eq 0) {
     if (Test-Path -Path "C:\Users\Public\Desktop") {
 		if (!(Test-Path -Path "C:\Windows\OEM\NetAppStorage.RDP")) {
-			(Get-Content C:\Users\Public\Desktop\NetAppStorage.RDP).Replace($ip,"$ip_port") | Set-Content C:\Users\Public\Desktop\NetAppStorage.RDP
-			#Move-Item -Path C:\Windows\OEM\NetAppStorage.RDP -Destination C:\Users\Public\Desktop -Force
+			write "$ip  $sqlName" | Out-File -FilePath C:\Windows\System32\drivers\etc\hosts -Append -Encoding ascii
+			#(Get-Content C:\Users\Public\Desktop\NetAppStorage.RDP).Replace($ip,"$ip_port") | Set-Content C:\Users\Public\Desktop\NetAppStorage.RDP
+				(Get-Content C:\Users\Public\Desktop\NetAppStorage.RDP).Replace($ip,"$sqlName") | Set-Content C:\Users\Public\Desktop\NetAppStorage.RDP
 			date >> C:\Windows\Panther\get.log
 			echo "NetAppStorage.RDP changed" >> C:\Windows\Panther\get.log
 				echo "Rename shortcut" >> C:\Windows\Panther\get.log
